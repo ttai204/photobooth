9ed3504a1d98bf04d3037e8c087d8722c05cfecd
𝙨𝙩𝙚𝙖𝙡𝙚𝙧 🍉 <ngthanhtailv@gmail.com>
2026-03-13 15:02:23 +0700

    Update PreviewCanvas.jsx

--- photobooth/components/PreviewCanvas.jsx
+++ photobooth/components/PreviewCanvas.jsx
@@ -154,7 +154,7 @@ export default function PreviewCanvas({ photos, frameSrc }) {
               <polyline points="7 10 12 15 17 10"/>
               <line x1="12" y1="15" x2="12" y2="3"/>
             </svg>
-            Download Photo Strip
+            Tải ảnh xuống
           </>
         )}
       </button>
